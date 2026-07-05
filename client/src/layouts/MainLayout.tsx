58c7fb9744469ac7d8874bbe05c0198b7bb045e4
Tim <pulseplay962@gmail.com>
2026-07-05 16:49:55 -0400

    Fix project structure

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
+import Navbar from "../components/Navbar";
 
 export default function MainLayout() {
   return (
@@ -10,8 +9,6 @@ export default function MainLayout() {
       <main className="flex-1">
         <Outlet />
       </main>
-
-      <Footer />
     </div>
   );
 }
